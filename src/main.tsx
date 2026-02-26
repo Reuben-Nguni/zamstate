3dc3f14343006a250d5a6650c12fa6121dc7f84c
Reuben Nguni <ngunireubenjr@gmail.com>
2026-02-26 02:48:20 +0200

    Remove unused registerServiceWorker import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
 import './styles/main.scss'
-import { registerServiceWorker, unregisterServiceWorker } from './registerServiceWorker'
+import { unregisterServiceWorker } from './registerServiceWorker'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
